a81298a9c37f8d57629090d0c3656e882c0bd2d5
Chancellion <ivanchenko160306@gmail.com>
2024-12-11 08:33:59 +0200

    Add project files.

---
+++ Lab-5/Task-1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+public abstract class Vehicle
+{
+    public int Speed { get; set; }
+    public int Capacity { get; set; }
+
+    public abstract void Move();
+}
+public class Human
+{
+    public int Speed { get; set; }
+
+    public void Move()
+    {
+        Console.WriteLine("Людина ходить зi швидкiстю " + Speed);
+    }
+}
+public class Car : Vehicle
+{
+    public override void Move()
+    {
+        Console.WriteLine("Машина їде зi швидкiстю " + Speed);
+    }
+}
+public class Bus : Vehicle
+{
+    public override void Move()
+    {
+        Console.WriteLine("Автобус їде зi швидкiстю " + Speed);
+    }
+}
+public class Train : Vehicle
+{
+    public override void Move()
+    {
+        Console.WriteLine("Трамвай їде зi швидкiстю " + Speed);
+    }
+}
+public class Route
+{
+    public string StartPoint { get; set; }
+    public string EndPoint { get; set; }
+
+    public Route(string startPoint, string endPoint)
+    {
+        StartPoint = startPoint;
+        EndPoint = endPoint;
+    }
+
+    public void CalculateOptimalRoute(Vehicle vehicle)
+    {
+        Console.WriteLine($"Обрахунок оптимального маршруту {vehicle.GetType().Name} з {StartPoint} до {EndPoint}");
+    }
+}
+public class TransportNetwork
+{
+    private List<Vehicle> vehicles = new List<Vehicle>();
+
+    public void AddVehicle(Vehicle vehicle)
+    {
+        vehicles.Add(vehicle);
+    }
+
+    public void ControlMovement()
+    {
+        foreach (var vehicle in vehicles)
+        {
+            vehicle.Move();
+        }
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Данiїл Iванченко, КIб-1-23-4.0д");
+        Car car = new Car { Speed = 100, Capacity = 4 };
+        Bus bus = new Bus { Speed = 60, Capacity = 40 };
+        Train train = new Train { Speed = 120, Capacity = 200 };
+        Human human = new Human { Speed = 5 };
+
+        Route route = new Route("Київ", "Львiв");
+
+        TransportNetwork network = new TransportNetwork();
+        network.AddVehicle(car);
+        network.AddVehicle(bus);
+        network.AddVehicle(train);
+
+        network.ControlMovement();
+
+        route.CalculateOptimalRoute(car);
+        route.CalculateOptimalRoute(bus);
+        route.CalculateOptimalRoute(train);
+
+        human.Move();
+    }
+}
---
+++ Task-2/Task-2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+public class MathOperations
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+    public int[] Add(int[] a, int[] b)
+    {
+        if (a.Length != b.Length)
+            throw new ArgumentException("Масиви повинні бути однакової довжини");
+
+        int[] result = new int[a.Length];
+        for (int i = 0; i < a.Length; i++)
+        {
+            result[i] = a[i] + b[i];
+        }
+        return result;
+    }
+    public int[,] Add(int[,] a, int[,] b)
+    {
+        if (a.GetLength(0) != b.GetLength(0) || a.GetLength(1) != b.GetLength(1))
+            throw new ArgumentException("Матриці повинні бути однакових розмірів");
+
+        int[,] result = new int[a.GetLength(0), a.GetLength(1)];
+        for (int i = 0; i < a.GetLength(0); i++)
+        {
+            for (int j = 0; j < a.GetLength(1); j++)
+            {
+                result[i, j] = a[i, j] + b[i, j];
+            }
+        }
+        return result;
+    }
+    public int[,,] Add(int[,,] a, int[,,] b)
+    {
+        if (a.GetLength(0) != b.GetLength(0) || a.GetLength(1) != b.GetLength(1) || a.GetLength(2) != b.GetLength(2))
+            throw new ArgumentException("Тензори повинні бути однакових розмірів");
+
+        int[,,] result = new int[a.GetLength(0), a.GetLength(1), a.GetLength(2)];
+        for (int i = 0; i < a.GetLength(0); i++)
+        {
+            for (int j = 0; j < a.GetLength(1); j++)
+            {
+                for (int k = 0; k < a.GetLength(2); k++)
+                {
+                    result[i, j, k] = a[i, j, k] + b[i, j, k];
+                }
+            }
+        }
+        return result;
+    }
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+    public int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Данiїл Iванченко, КIб-1-23-4.0д");
+        MathOperations mathOps = new MathOperations();
+        int sum = mathOps.Add(565, 342);
+        Console.WriteLine("Сума чисел: " + sum);
+
+        int[] array1 = { 15, 25, 64 };
+        int[] array2 = { 44, 51, 11 };
+        int[] arraySum = mathOps.Add(array1, array2);
+        Console.WriteLine("Сума масивів: " + string.Join(", ", arraySum));
+    }
+}
